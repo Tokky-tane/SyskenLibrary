a2d527580808dbf622496885a4a2ccc0f0ad0b95
Tokky-tane <tokky_tane@outlook.com>
2020-01-01 19:43:24 +0900

    add database migration

---
+++ migrations/20200101103549-create-book.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      isbn: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Books');
+  }
+};
---
+++ models/book.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    isbn: DataTypes.INTEGER
+  }, {});
+  Book.associate = function(models) {
+    // associations can be defined here
+  };
+  return Book;
+};
