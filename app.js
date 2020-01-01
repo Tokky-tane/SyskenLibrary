d8321c84d0b6e09e2cc40280c3c64235ace88370
Tokky-tane <tokky_tane@outlook.com>
2020-01-01 20:18:29 +0900

    add error handling

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ db = require('./models')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/books', function (req, res) {
+app.get('/books', function (req, res, next) {
     db.Book.findAll({ attributes: ['title', 'author', 'isbn'] })
         .then(books => {
             res.send(books)
+        })
+        .catch(err => {
+            next(err)
         })
 })
 
