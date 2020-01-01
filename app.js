22f392dbb2d388f72c39a2df7c7c30959f3ef224
Tokky-tane <tokky_tane@outlook.com>
2020-01-01 20:07:01 +0900

    add getting all books

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+db = require('./models')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/books', function (req, res) {
+    db.Book.findAll({ attributes: ['title', 'author', 'isbn'] })
+        .then(books => {
+            res.send(books)
+        })
+})
 
 app.listen(port, () => console.log(`Sysken Library App listening on port ${port}!`))
