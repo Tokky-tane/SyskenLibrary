d998598deb2ed11c7d069024e6bff8c146fe315b
Tokky-tane <tokky_tane@outlook.com>
2020-01-02 10:40:09 +0900

    retruning json instead of array

--- app.js
+++ app.js
@@ -10,11 +10,17 @@ app.use(express.urlencoded({ extended: true }))
 app.get('/books', function (req, res, next) {
     db.Book.findAll({ attributes: ['title', 'author', 'isbn'] })
         .then(books => {
-            res.send(books)
+            res.json(addObjectName(books, 'books'))
         })
         .catch(err => {
             next(err)
         })
 })
+
+function addObjectName(object, name) {
+    var emit_object = {}
+    emit_object[name] = object
+    return emit_object
+}
 
 app.listen(port, () => console.log(`Sysken Library App listening on port ${port}!`))
