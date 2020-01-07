53c221032f7a49a8897a2a020cc00d69f00a5b2f
Tokky-tane <tokky_tane@outlook.com>
2020-01-07 22:40:26 +0900

    rename db -> models

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-db = require('./models')
+const models = require('./models')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/books', function (req, res, next) {
-    db.Book.findAll({ attributes: ['title', 'author', 'isbn'] })
+    models.Book.findAll({ attributes: ['title', 'author', 'isbn'] })
         .then(books => {
             res.json(addObjectName(books, 'books'))
         })
@@ -20,7 +20,7 @@ app.get('/books', function (req, res, next) {
 app.post('/books', (req, res, next) => {
     let new_book = req.body
 
-    db.Book.create({
+    models.Book.create({
         title: new_book.title,
         author: new_book.author,
         isbn: new_book.isbn
