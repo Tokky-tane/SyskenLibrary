f10873bf5c9500d538ae98121ab7468ac9184997
Tokky-tane <tokky_tane@outlook.com>
2020-01-09 16:06:03 +0900

    add access-control-allow-origin header

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ db = require('./models')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    next()
+})
+
 app.get('/books', function (req, res, next) {
     db.Book.findAll({ attributes: ['title', 'author', 'isbn'] })
         .then(books => {
