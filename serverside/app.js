21323438168c124910976a231d529e7bc995a16c
Tokky_tane <tokky_tane@outlook.com>
2020-01-17 21:18:01 +0900

    fix require path

--- serverside/app.js
+++ serverside/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 const { check, validationResult } = require('express-validator')
-const models = require('./models')
+const models = require('./database/models')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
