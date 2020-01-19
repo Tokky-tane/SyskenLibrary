31fc159950b8a1b39253b814dc0515a44d962647
Tokky_tane <tokky_tane@outlook.com>
2020-01-19 10:54:30 +0900

    move books router declaration

--- server/app.js
+++ server/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const books = require('./routes/books');
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
 
+const books = require('./routes/books');
 app.use('/books', books);
 
 app.listen(port,
