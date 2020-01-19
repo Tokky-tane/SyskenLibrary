2a67ed2e1b9ca5c4f8d245b8a85fa828b5de6665
Tokky_tane <tokky_tane@outlook.com>
2020-01-19 10:50:15 +0900

    fix cors issue

--- server/app.js
+++ server/app.js
@@ -4,12 +4,12 @@ const port = 3001;
 
 const books = require('./routes/books');
 
-app.use('/books', books);
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
+
+app.use('/books', books);
 
 app.listen(port,
     () => console.log(`Sysken Library App listening on port ${port}!`));
