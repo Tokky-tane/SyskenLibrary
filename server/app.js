67d0f2f623de9491c5c9f3039ee8baa2098474cf
Tokky-tane <tokky_tane@outlook.com>
2020-01-25 12:44:12 +0900

    allow to post json

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const port = 3001;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
