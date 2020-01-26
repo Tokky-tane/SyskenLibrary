13515f9223e54266d20eca611b0a4f87a4051f03
Tokky-tane <tokky_tane@outlook.com>
2020-01-26 18:02:44 +0900

    changed app.js to module

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3001;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -11,5 +10,4 @@ app.use((req, res, next) => {
 const books = require('./routes/books');
 app.use('/books', books);
 
-app.listen(port,
-    () => console.log(`Sysken Library App listening on port ${port}!`));
+module.exports = app;
---
+++ server/index.js
@@ -0,0 +1,5 @@
+const port=3001;
+const app= require('./app');
+
+app.listen(port,
+    () => console.log(`Sysken Library App listening on port ${port}!`));
