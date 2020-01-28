1f62c56b4cf20bb69b7b8d08ab0ecaf71fe0e98b
Tokky-tane <tokky_tane@outlook.com>
2020-01-28 16:53:01 +0900

    rename index.js -> server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const port=3001;
-const app= require('./app');
+const port = 3001;
+const app = require('./app');
 
 app.listen(port,
     () => console.log(`Sysken Library App listening on port ${port}!`));
