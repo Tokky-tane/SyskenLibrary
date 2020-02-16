6b292b8f948c716af44f6e154e95fbb83c18c547
Tokky-tane <tokky_tane@outlook.com>
2020-02-16 20:00:46 +0900

    add registering user

---
+++ server/routes/users.js
@@ -0,0 +1,40 @@
+const express=require('express');
+const router=require('express-promise-router')();
+const {check, validationResult} = require('express-validator');
+const bcrypt=require('bcrypt');
+const user=require('../models').User;
+
+router.use(express.json());
+router.use(express.urlencoded({extended: true}));
+
+router.post('/', [
+  check('email').isEmail(),
+  check('password').isLength({min: 5, max: 30}),
+], (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors);
+  }
+
+  const resevedUser=req.body;
+  const saltRounds=10;
+  bcrypt.hash(resevedUser.password, saltRounds)
+      .then((hash)=>user.findOrCreate({
+        where: {
+          email: resevedUser.email,
+        },
+        defaults: {password: hash},
+      }))
+      .then((createdUser, created)=>{
+        if (!created) {
+          return res.status(409).end();
+        }
+
+        const createdUserUrl = req.protocol + '://' + req.get('host') + req.url + `/${createdUser.id}`;
+        return res.location(createdUserUrl)
+            .status(201)
+            .end();
+      });
+});
+
+module.exports = router;
