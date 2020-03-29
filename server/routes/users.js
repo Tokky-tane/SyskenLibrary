b6cac4dbd8f86f4c325f95900ad4f732c874c162
Tokky-tane <tokky_tane@outlook.com>
2020-03-29 15:39:34 +0900

    fix typo

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require('express-promise-router')();
 const {check} = require('express-validator');
 const user = require('../models').User;
-const varidate = require('../middleware/validation');
+const validate = require('../middleware/validation');
 const users = require('../utils/users');
 
 router.use(express.json());
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 router.post('/', [
   check('email').isEmail(),
   check('password').isLength({min: 5, max: 30}),
-], varidate, async (req, res, next) => {
+], validate, async (req, res, next) => {
   const resevedUser = req.body;
 
   const createdUser = await users.create(
